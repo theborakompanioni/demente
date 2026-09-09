227af86d537ef1df101abcfd17f7ae23a22bcb63
theborakompanioni <theborakompanioni+github@gmail.com>
2026-09-09 15:47:36 +0200

    chore: remove @EnableWebMvc

--- demented/src/main/java/org/tbk/nostr/demented/DementedWebMvcConfigurer.java
+++ demented/src/main/java/org/tbk/nostr/demented/DementedWebMvcConfigurer.java
@@ -2,11 +2,9 @@ package org.tbk.nostr.demented;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @ConditionalOnWebApplication
 @Configuration(proxyBeanMethods = false)
 class DementedWebMvcConfigurer implements WebMvcConfigurer {
